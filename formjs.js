6f20e166acef5420177cb0529ec581525ebd603a
Saikumar-starboy <88478772+Saikumar-starboy@users.noreply.github.com>
2023-04-04 22:07:34 +0530

    Delete data from Local Storage

--- formjs.js
+++ formjs.js
@@ -14,6 +14,15 @@ function showUserOnScreen(obj){
     const parentElem = document.getElementById('users')
     const childElem = document.createElement('li')
     childElem.textContent = obj.name + ' - ' + obj.email 
-     parentElem.appendChild(childElem)
+
+    const deleteButton = document.createElement('input')
+    deleteButton.type = 'button'
+    deleteButton.value = 'Delete'
+    deleteButton.onclick = () => {
+        localStorage.removeItem(obj.email)
+        parentElem.removeChild(childElem)
+    }
+    childElem.appendChild(deleteButton)
+    parentElem.appendChild(childElem)
 
 }
